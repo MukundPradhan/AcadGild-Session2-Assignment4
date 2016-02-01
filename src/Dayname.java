ee6924c0d3653b0e0df7eb2ae54ce66340c40112
MukundPradhan <replyme.mmp@gmail.com>
2016-02-01 11:25:59 +0530

    Update

--- src/Dayname.java
+++ src/Dayname.java
@@ -2,22 +2,53 @@
 public class Dayname {
 
 	/**
-	 * Program for to write the Day Name in the month of January - 2016
+	 * Program for to write the Day Name in the given month 
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String Array[]={"Mon","Tue","Wed","Thus","Fri","Sat","Sun"};
-
-		int Date, DaySr=4;
-		System.out.println("   Date  " + "\t" + "Date Name");
-		System.out.println("---------\t---------");
-		for(Date=1;Date<=31;Date++){
-			System.out.println(Date+"-Jan-2016" + "\t" + Array[DaySr] );
-			DaySr++;
-			if (DaySr >6){
-				DaySr=0;
-			}
-		}
+		int monthName;
+		monthName=4;
+		
+		switch(monthName){
+		case 1:
+			System.out.println("The total no of days in January Month is 31");
+			break;
+			
+		case 2:
+			System.out.println("The total no of days in February Month is 29");
+			break;
+			
+		case 3:
+			System.out.println("The total no of days in March is 31");
+			break;
+		case 4:
+			System.out.println("The total no of days in April is 30");
+			break;
+		case 5:
+			System.out.println("The total no of days in May is 31");
+			break;
+		case 6:
+			System.out.println("The total no of days in June is 30");
+			break;
+		case 7:
+			System.out.println("The total no of days in July is 31");
+			break;
+		case 8:
+			System.out.println("The total no of days in August is 31");
+			break;
+		case 9:
+			System.out.println("The total no of days in September is 30");
+			break;
+		case 10:
+			System.out.println("The total no of days in October is 31");
+			break;
+		case 11:
+			System.out.println("The total no of days in November is 30");
+			break;
+		case 12:
+			System.out.println("The total no of days in December is 31");
+		}	
+		
 		
 	}
 
